deab9b51c13398fbb2b895161045ec70a0d89185
李大熊 <leebear6928@gmail.com>
2020-03-01 19:08:26 +0800

    舊資料測試連結

---
+++ line-bot-tutorial-master/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('FuZKg3b2JlYT4lz9IbRwzaTN8catXSGYHVynmnpaYNag84VGbHq1WBVeQTwu5Gs1FJiFW61rAXw70qWtQHobjcSokG4gHAKJ/XuupgUH+g+AF0PQYR35alujGq3u7yU9INuPc5EPBwgJspz/2qmc9QdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('6f53d90a99d85e8e39ddb6786846ba46')
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print(event)
+    text=event.message.text
+
+    if (text=="Hi"):
+        reply_text = "Hello"
+        #Your user ID
+
+    elif(text=="你好"):
+        reply_text = "哈囉"
+    elif(text=="機器人"):
+        reply_text = "叫我嗎"
+    else:
+        reply_text = text
+#如果非以上的選項，就會學你說話
+    message = TextSendMessage(reply_text)
+    line_bot_api.reply_message(event.reply_token, message)
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
